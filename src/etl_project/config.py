1618ef472131eb4029596bce8d7b33b48337f3cd
andresbpaz <andabopa@gmail.com>
2025-10-02 00:02:27 -0500

    Añadido archivo runtime.txt y requirements.txt, y mejoras en la configuración de la base de datos en config.py para utilizar secretos de Streamlit. Actualización en el flujo de trabajo ETL.

--- src/etl_project/config.py
+++ src/etl_project/config.py
@@ -1,6 +1,7 @@
 import os
 import yaml
 from pathlib import Path
+import os, streamlit as st
 
 class Config:
     def __init__(self, yaml_file="config/settings.yaml"):
@@ -11,11 +12,11 @@ class Config:
 
         # Variables de la base de datos
         db = self.config.get("database", {})
-        self.DB_HOST =  os.getenv("DB_HOST", db.get("HOST")) 
-        self.DB_PORT = os.getenv("DB_PORT", db.get("PORT")) 
-        self.DB_NAME = os.getenv("DB_NAME", db.get("DB_NAME")) 
-        self.DB_USER = os.getenv("DB_USER", db.get("USER")) 
-        self.DB_PASSWORD = os.getenv("DB_PASSW", db.get("PASSWORD")) 
+        self.DB_HOST =  os.getenv("DB_HOST", db.get("HOST")) or st.secrets.get("DB_HOST")
+        self.DB_PORT = os.getenv("DB_PORT", db.get("PORT")) or st.secrets.get("DB_PORT", "5432")
+        self.DB_NAME = os.getenv("DB_NAME", db.get("DB_NAME")) or st.secrets.get("DB_NAME")
+        self.DB_USER = os.getenv("DB_USER", db.get("USER"))  or st.secrets.get("DB_USER")
+        self.DB_PASSWORD = os.getenv("DB_PASSW", db.get("PASSWORD")) or st.secrets.get("DB_PASSW")
 
         # Rutas de archivos
         pat = self.config.get("paths", {})
